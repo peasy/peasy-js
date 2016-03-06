2f20ead99eabf4e8b53f6606816914154f99ec32
ahanusa <ahanusa@gettyimages.com>
2016-03-05 20:17:07 -0600

    add specs for getAllCommand and associated methods

--- spec/businessServiceSpec.js
+++ spec/businessServiceSpec.js
@@ -2,61 +2,90 @@ describe("BusinessService", function() {
   var BusinessService = require('../src/businessService');
   var Command = require('../src/command');
   var DataProxy = require('../src/dataProxy');
+  var service, command, dataProxy;
 
-  it("returns a new instance when invoked directly", function() {
-    var service = BusinessService();
-    expect(service instanceof BusinessService).toBeTruthy();
+  describe("constructor", () => {
+    it("returns a new instance when invoked directly", function() {
+      var service = BusinessService();
+      expect(service instanceof BusinessService).toBe(true);
+    });
+
+    it("returns a new instance when instantiated", function() {
+      var service = new BusinessService();
+      expect(service instanceof BusinessService).toBe(true);
+    });
   });
 
-  it("returns a new instance when instantiated", function() {
-    var service = new BusinessService();
-    expect(service instanceof BusinessService).toBeTruthy();
-  });
-
-  describe("getAllCommand", function() {
-    var service, command, dataProxy;
+  describe("getAllCommand and associated methods", function() {
 
     beforeAll(() => {
-      callback = function() {};
       dataProxy = new DataProxy();
       service = new BusinessService(dataProxy);
       command = service.getAllCommand();
-      spyOn(service, "__onGetAllCommandInitialization").and.callFake(function(context, done) {
-        context.foo = "bar";
-        done();
-      });
-      /*spyOn(service, "__onGetAllCommandInitialization").and.callThrough();*/
-      spyOn(service, "__getRulesForGetAll").and.callFake(function(context) {
-        console.log("MADE IT!!!", context);
-        context.foo = "meh";
-      });
-      spyOn(service, "__getAll");
-      command.execute(callback);
-      /*spyOn(dataProxy, "getAll").and.returnValue([]);*/
+      spyOn(dataProxy, "getAll").and.returnValue([]);
     });
-
-    /*it("should be defined", () => {*/
-      /*expect(service.getAllCommand).not.toBeUndefined();*/
-    /*});*/
 
     describe("the returned command", () => {
       it("is of the correct type", () => {
         expect(command instanceof Command).toBe(true);
       });
 
-      describe("on command execution", () => {
+      describe("on execution", () => {
         it("invokes service.__onGetAllCommandInitialization", function() {
+          spyOn(service, "__onGetAllCommandInitialization");
+          command.execute(() => {});
           expect(service.__onGetAllCommandInitialization)
             .toHaveBeenCalledWith(jasmine.any(Object), jasmine.any(Function));
         });
 
         it("invokes service.__getRulesForGetAll", function() {
-          expect(service.__getRulesForGetAll).toHaveBeenCalledWith({ foo: "bar"});
+          spyOn(service, "__getRulesForGetAll").and.callThrough();
+          command.execute(() => {});
+          expect(service.__getRulesForGetAll)
+            .toHaveBeenCalledWith(jasmine.any(Object));
         });
 
         it("invokes service.__getAll", function() {
+          spyOn(service, "__getAll");
+          command.execute(() => {});
           expect(service.__getAll)
-            .toHaveBeenCalledWith({ foo: "bar" }, jasmine.any(Function));
+            .toHaveBeenCalledWith(jasmine.any(Object), jasmine.any(Function));
+        });
+
+        it("passes shared context to all getAll pipeline methods", () => {
+          var TestService = function() {};
+          var sharedContext;
+          TestService.prototype = new BusinessService();
+          TestService.prototype.__onGetAllCommandInitialization = (context, done) => {
+            context.foo = "";
+            done();
+          };
+          TestService.prototype.__getRulesForGetAll = (context) => {
+            context.bar = "";
+            return [];
+          };
+          TestService.prototype.__getAll = (context, done) => {
+            sharedContext = context;
+            done();
+          }
+          var command = new TestService(dataProxy).getAllCommand();
+          command.execute(() => { });
+          expect(sharedContext.foo).not.toBeUndefined();
+          expect(sharedContext.bar).not.toBeUndefined();
+        });
+      });
+    });
+
+    describe("instance methods", () => {
+      describe("__getAll", function() {
+        it("invokes dataProxy.getAll", () => {
+          expect(dataProxy.getAll).toHaveBeenCalled();
+        });
+      });
+
+      describe("__getRulesForGetAll", () => {
+        it("returns an empty array", () => {
+          expect(service.__getRulesForGetAll()).toEqual([]);
         });
       });
     });
