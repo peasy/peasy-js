edbcf4091efc84bbd46223d28b9ffaba8d9dc5f7
ahanusa <ahanusa@gettyimages.com>
2016-03-08 08:46:10 -0600

    add tests for getById

--- spec/businessServiceSpec.js
+++ spec/businessServiceSpec.js
@@ -71,8 +71,65 @@ describe("BusinessService", function() {
         });
       });
     });
-
-
   });
 
+  describe("getByIdCommand and associated methods", function() {
+
+    beforeAll(() => {
+      dataProxy = new DataProxy();
+      service = new BusinessService(dataProxy);
+      command = service.getByIdCommand();
+      spyOn(dataProxy, "getById").and.returnValue([]);
+    });
+
+    describe("instance methods", () => {
+      describe("__getById", () => {
+        it("invokes dataProxy.getById", () => {
+          command.execute(() => {});
+          expect(dataProxy.getById).toHaveBeenCalled();
+        });
+      });
+
+      describe("__getRulesForGetById", () => {
+        it("returns an empty array", () => {
+          var callbackValue;
+          var id = 1;
+          service.__getRulesForGetById(id, {}, (result) => callbackValue = result); 
+          expect(callbackValue).toEqual([]);
+        });
+      });
+    });
+
+    describe("the returned command", () => {
+      it("is of the correct type", () => {
+        expect(command instanceof Command).toBe(true);
+      });
+
+      describe("on execution", () => {
+        it("passes shared context and id to all getById pipeline methods", () => {
+          var TestService = function() {};
+          var sharedContext;
+          TestService.prototype = new BusinessService();
+          TestService.prototype.__onGetByIdCommandInitialization = (id, context, done) => {
+            context.ids = 1;
+            done();
+          };
+          TestService.prototype.__getRulesForGetById = (id, context, done) => {
+            context.ids++; 
+            done([]);
+          };
+          TestService.prototype.__getById = (id, context, done) => {
+            context.ids++; 
+            sharedContext = context;
+            done();
+          }
+          var id = 1;
+          var command = new TestService(dataProxy).getByIdCommand(1);
+          command.execute(() => { });
+          expect(sharedContext.ids).not.toBeUndefined();
+          expect(sharedContext.ids).toEqual(3);
+        });
+      });
+    });
+  });
 });
