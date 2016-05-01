a72b3b07eadc0af29f1c60cbc72b0c4af98498cf
Aaron Hanusa <aaron.hanusa@gmail.com>
2016-05-01 14:56:19 -0500

    add leading semicolon to avoid potential minification issues

--- src/peasyjs.js
+++ src/peasyjs.js
@@ -1,4 +1,4 @@
-(function(root, factory) {
+;(function(root, factory) {
   if (typeof define === "function" && define.amd) {
     define(["peasyjs"], factory);
   } else if (typeof module === "object" && module.exports) {
