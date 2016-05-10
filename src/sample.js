f03f1c4b1297b399e53726a7af83ba7ce59e30c0
ahanusa <aaron.hanusa@gmail.com>
2016-05-09 19:22:07 -0500

    add comments

--- src/sample.js
+++ src/sample.js
@@ -4,6 +4,8 @@ var Rule = require('./peasy').Rule;
 var BusinessService = require('./peasy').BusinessService;
 var Command = require('./peasy').Command;
 
+// CREATE RULES
+
 var AgeRule = Rule.extend({
   association: "age",
   params: ['birthdate'],
@@ -12,7 +14,7 @@ var AgeRule = Rule.extend({
       this.__invalidate("You are too young");
     }
     var time = Math.floor((Math.random() * 3000) + 1);
-    setTimeout(() => done(this), time);
+    setTimeout(() => done(this), time); // simulate latency
   }
 });
 
@@ -24,7 +26,7 @@ var NameRule = Rule.extend({
       this.__invalidate("Name cannot be Aaron");
     }
     var time = Math.floor((Math.random() * 3000) + 1);
-    setTimeout(() => done(this), time);
+    setTimeout(() => done(this), time); // simulate latency
   }
 });
 
@@ -36,15 +38,18 @@ var FieldRequiredRule = Rule.extend({
       this.__invalidate(this.field + " is required");
     }
     var time = Math.floor((Math.random() * 3000) + 1);
-    setTimeout(() => done(this), time);
+    setTimeout(() => done(this), time); // simulate latency
   }
 });
 
-var PersonService = BusinessService.extend({ 
+// CREATE SERVICE AND WIRE UP VALIDATION/BUSINESS LOGIC FOR INSERT
+
+var PersonService = BusinessService.extend({
   functions: [{
     '__getRulesForInsert': getRulesForInsert
   }]
 }).service;
+
 
 function getRulesForInsert(person, context, done) {
 
@@ -55,40 +60,57 @@ function getRulesForInsert(person, context, done) {
   //]);
 
   done([new AgeRule(person.age)
-                .ifValidThenExecute(() => console.log("Age succeeded"))
-                .ifInvalidThenExecute(() => console.log("Age failed"))
-                .ifValidThenValidate(new NameRule(person.name)
-                                          .ifValidThenExecute(() => console.log("Name succeeeded"))
-                                          .ifInvalidThenExecute(() => console.log("Name failed"))
-                                          .ifValidThenValidate(new FieldRequiredRule("address", person)
+              .ifValidThenExecute(() => console.log("Age succeeded"))
+              .ifInvalidThenExecute(() => console.log("Age failed"))
+              .ifValidThenValidate(new NameRule(person.name)
+                                         .ifValidThenExecute(() => console.log("Name succeeeded"))
+                                         .ifInvalidThenExecute(() => console.log("Name failed"))
+                                         .ifValidThenValidate(new FieldRequiredRule("address", person)
                                                                     .ifValidThenExecute(() => console.log("Address succeeeded"))
                                                                     .ifInvalidThenExecute(() => console.log("Address failed"))
                                                               ))]);
 }
 
-var PersonDataProxy = function() { }
+// CREATE AN IN-MEMORY DATA PROXY (this could be duck typed angular resource, react store, etc.)
+
+var PersonDataProxy = function() {
+  this.data = [
+    { id: 1, name: "James Hendrix" },
+    { id: 2, name: "James Page" },
+    { id: 3, name: "David Gilmour" },
+  ];
+}
 
 PersonDataProxy.prototype = {
   constructor: PersonDataProxy,
   insert: function(data, done) {
-    data.id = 5;
+    var nextId = this.data.length + 1;
+    data.id = nextId;
+    this.data.push(data);
     done(data);
   }
 };
 
-var service = new PersonService(new PersonDataProxy());
+var proxy = new PersonDataProxy();
+var service = new PersonService(proxy);
 
-[
+var commands = [
   service.insertCommand({name: "Aaron", age: new Date('2/3/1975')}),
   service.insertCommand({name: "Aarons", age: new Date('2/3/1975'), address: 'aa'}),
   service.insertCommand({name: "Aaron", age: new Date('2/3/1925'), address: 'aa'}),
   service.insertCommand({name: "Aarons", age: new Date('2/3/1925')}),
   service.insertCommand({name: "aAaron", age: new Date('2/3/1925'), address: 'aaa'})
-]
-.forEach(function(command) {
+];
+
+commands.forEach(function(command, index) {
   command.execute((result) => {
-    console.log('---------------');
+    console.log('\n---------------');
     console.log(result);
+
+    if (index == commands.length - 1) {
+      console.log('\n---------------');
+      console.log("End Result", proxy.data);
+    }
   });
 });
 
